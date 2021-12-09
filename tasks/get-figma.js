4bcba6ad1f3f9190e01137d726fcf8011b3a6a58
Kevin.Krug <kevin.krug@aperto.com>
2021-12-09 15:38:56 +0100

    :wrench: adds getColors fct to get-figma task

--- tasks/get-figma.js
+++ tasks/get-figma.js
@@ -13,15 +13,43 @@ async function getFigmaObjTree(apiKey=figmaApiKey, figmaId ="Mi7wdQqRHavaPODG1EJ
     return res.data;
 }
 
-
-const getPageStyles = (figmaObjTree, pageName) => figmaObjTree.document.children.filter(item => {
-    return item.name === pageName;
+const getArtboardStyles = (parentArtboard, artboardName) => parentArtboard.children.filter(item => {
+    return item.name === artboardName;
 })[0].children;
 
+const getPageStyles = (figmaObjTree, pageName) => getArtboardStyles(figmaObjTree.document, pageName);
 
+const coverArtboard = getPageStyles(await getFigmaObjTree(), "Cover")[0];
+
+const getColors = (artboard) => {
+    const paletteArtboard = getArtboardStyles(artboard, 'Palette');
+
+    const colors = paletteArtboard.reduce((acc, item) => {
+        const getRGBValue = (colorKey) => item.children[1].fills[0].color[colorKey] * 255;
+
+        return {
+            ...acc,
+            [item.name]: {
+                value: `rgba(${getRGBValue("r")}, ${getRGBValue("g")}, ${getRGBValue(
+                    "b"
+                )}, ${item.children[1].fills[0].color.a})`,
+                type: "color"
+            }
+        };
+    }, {});
+
+    return colors;
+}
+
+<<<<<<< HEAD
 if (!figmaApiKey) {
 	console.log('* Please add add your figma API key: yarn run getfigma <YOURFIGMAAPIKEY>');
 } else {
     console.log(getPageStyles(await getFigmaObjTree(), "Cover"))
 }
 
+=======
+const colors = getColors(coverArtboard)
+
+console.log(colors);
+>>>>>>> 0233db9 (:wrench: adds getColors fct to get-figma task)
